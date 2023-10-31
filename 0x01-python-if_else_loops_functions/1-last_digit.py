0a472e9d0641291c8b71ac1027d49644d08061a4
GodwinCyber <godwinchuks032@gmail.com>
2023-10-31 07:08:07 -0700

    assign a random signed number to the variable number each time it is executed

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ last_digit = abs(number) % 10
 
 if number < 0:
     last_digit = -last_digit
-    message = "and is lss than 6 and not 0"
+    message = "and is less than 6 and not 0"
 elif last_digit > 5:
     message = "and is greater than 5"
 elif last_digit == 0:
