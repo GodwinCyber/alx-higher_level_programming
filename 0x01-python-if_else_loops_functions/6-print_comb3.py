76d3a5b5f7e9f3473e77a38a1ed2736d73ab35c8
GodwinCyber <godwinchuks032@gmail.com>
2023-10-31 12:39:40 -0700

    program that prints all possible different combinations of two digits.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
-combinations = []
+count = 0
 
-for i in range(10):
-    for j in range(i + 1, 10):
-        combinations.append("{}{}".format(i, j))
-res = ', '.join(combinations)
-print(res)
+for i in range(9):
+    count += 1
+    for j in range(i * 10 + count, i * 10 + 10):
+        if j == 89:
+            print(89)
+        else:
+            print("{:02d}, ".format(j), end='')
