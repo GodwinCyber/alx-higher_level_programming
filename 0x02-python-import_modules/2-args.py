1c5b2eca0f10b57d0452a70faf48dfd9a3cc4f6a
GodwinCyber <godwinchuks032@gmail.com>
2023-11-02 10:05:36 -0700

    number of agrument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,10 @@
 import sys
 
 def print_arguments():
-    num_argument = len(sys.argv) -1
-    print("{} arguments".format(num_argument))
+    num_arguments = len(sys.argv) -1
+    print("{} argument".format(num_arguments,
+        "s" if num_arguments != 1 else "", ":"
+        if num_arguments > 0 else "."))
 
     for i in range(1, len(sys.argv)):
         argument = sys.argv[i]
