8f17bcee3809a77435ea86e9755d7fd25caab10f
GodwinCyber <godwinchuks032@gmail.com>
2023-11-02 10:26:12 -0700

    ts the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,15 +2,18 @@
 
 import sys
 
-def print_arguments():
-    num_arguments = len(sys.argv) -1
-    print("{} argument".format(num_arguments,
-        "s" if num_arguments != 1 else "", ":"
-        if num_arguments > 0 else "."))
+def main():
+    count = len(sys.argv) - 1
 
-    for i in range(1, len(sys.argv)):
-        argument = sys.argv[i]
-        print("{}: {}".format(i, argument))
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
 if __name__ == "__main__":
-    print_arguments()
+    main()
