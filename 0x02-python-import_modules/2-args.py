062d1a9bc2fed37fb0fa9d82685c2d8c13aff45b
GodwinCyber <godwinchuks032@gmail.com>
2023-11-02 13:55:29 -0700

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,14 @@
 
 import sys
 
-    total_count = len(sys.argv) - 1
+    count = len(sys.argv) - 1
 
-    if total_count == 0:
+    if count == 0:
         print("0 arguments.")
-    elif total_count == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(total_count))
+        print("{} arguments:".format(count))
 
-    for i in range(total_count):
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
