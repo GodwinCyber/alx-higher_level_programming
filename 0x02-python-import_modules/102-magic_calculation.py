ed626335874eb9d60b341cdf6f64fff951dc74fa
GodwinCyber <godwinchuks032@gmail.com>
2023-11-02 19:38:32 -0700

    Python function def magic_calculation(a, b)

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    add = None
-    sub = None
+
+    from magic_calculation_102 import add, sub
 
     if a < b:
-        add, sub = __import__('magic_calculation_102', globals(), locals(), ('add', 'sub'), 0)
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
+        return (c)
     else:
-        sub = __import__('magic_calculation_102', globals(), locals(), ('add', 'sub'), 0)
-        c = sub(a, b)
+        return (sub(a, b))
 
     return c
