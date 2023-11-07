0e0f838b345ab38b12cb3df9ad5110268d13b3e3
GodwinCyber <godwinchuks032@gmail.com>
2023-11-06 17:57:24 -0800

    checks if a singly linked list is a palindrome.

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * compare_lists - compares two linked lists
+ * @list1: pointer to the first linked list
+ * @list2: pointer to the second linked list
+ * Return: 1 if lists are the same, 0 if not
+ */
+int compare_lists(listint_t *list1, listint_t *list2)
+{
+	while (list1 != NULL && list2 != NULL)
+	{
+		if (list1->n != list2->n)
+			return 0;
+		list1 = list1->next;
+		list2 = list2->next;
+	}
+
+	return (list1 == NULL && list2 == NULL);
+}
+
+/**
+ * reverse_list - reverses a linked list
+ * @head: pointer to the head of the linked list
+ */
+void reverse_list(listint_t **head)
+{
+	listint_t *prev = NULL;
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+
+	*head = prev;
+}
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: pointer to the head of the linked list
+ * Return: 1 if it's a palindrome, 0 if not
+ */
+int is_palindrome(listint_t **head)
+{
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
+
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *second_half;
+	int is_palindrome = 1;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		prev = slow;
+		slow = slow->next;
+	}
+
+	if (fast != NULL)
+		slow = slow->next;
+
+	second_half = slow;
+	prev->next = NULL;
+	reverse_list(&second_half);
+
+	is_palindrome = compare_lists(*head, second_half);
+
+	reverse_list(&second_half);
+	if (prev != NULL)
+		prev->next = second_half;
+	else
+		*head = second_half;
+
+	return is_palindrome;
+}
