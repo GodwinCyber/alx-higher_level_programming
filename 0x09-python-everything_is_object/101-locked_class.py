3eb9ab9bf2f72ae9f2d01a905867ea2fab573100
GodwinCyber <godwinchuks032@gmail.com>
2023-11-28 08:17:02 -0800

    Write a class LockedClass with no class or object attribute, that prevents the user from dynamically creating new instance attributes

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-class Lockedclass:
-    def __slots__ = ("first_name",)
+"""LockedClass module"""
+class LockedClass:
+    __slots__ = ("first_name",)
 
     def __init__(self, first_name=""):
+        """initializing the first_name attribute"""
         self.first_name = first_name
