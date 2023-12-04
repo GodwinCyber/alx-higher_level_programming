b24d0493fbde23b786e17104a8b1cd8eece05ba2
GodwinCyber <godwinchuks032@gmail.com>
2023-12-04 12:03:20 -0800

    return the list pof attribute and method of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""define avialable attr and method
+of an object
+"""
+
+
+def lookup(obj):
+    """return theblist of avilable attri and
+    method of an object
+    """
+    return (dir(obj))
