a47f726d59e876753874206b45b3a95079571292
GodwinCyber <godwinchuks032@gmail.com>
2023-12-05 01:39:39 -0800

    Full rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inheruted from BaseGeometry"""
+    def __init__(self, width, height):
+        """initailization"""
+        super().integer_validator("height", width)
+        super().integer_validator("width", height)
+        self.__height = height
+        self.__width = width
+
+    def area(self):
+        """impleted the area"""
+        return self.__height * self.__width
+
+    def __str__(self):
+        """return the rectangle description"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
