442af3a9999b204de313a47d79920b27a3f4d0df
GodwinCyber <godwinchuks032@gmail.com>
2023-12-05 17:47:45 -0800

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Define student-class"""
+
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialise new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance:"""
+        for k, v in json.items():
+            setattr(self, k, v)
