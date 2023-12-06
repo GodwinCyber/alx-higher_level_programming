765788682d4600b948ad85301e2a0c99c10e7c94
GodwinCyber <godwinchuks032@gmail.com>
2023-12-05 17:25:16 -0800

    Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define student-class"""
+
+class Student:
+    """Represent a student"""
+    def __init__(self, first_name, last_name, age):
+        """initialise new student"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance"""
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+        else:
+            return self.__dict__
