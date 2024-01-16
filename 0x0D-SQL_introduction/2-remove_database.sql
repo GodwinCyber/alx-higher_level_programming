910a07e471a9da72fd16158e08d8cfb46db3f74b
GodwinCyber <godwinchuks032@gmail.com>
2024-01-16 06:17:28 -0800

    Removed the newly created database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- delete the database hbtn_0c_0 in MySQL server
+-- the script will should not fail even if hbtn_0c_0 already exist in MySQL
+-- SELECT and SHOW statement are not allowed
+DROP DATABASE IF EXISTS hbtn_0c_0;
