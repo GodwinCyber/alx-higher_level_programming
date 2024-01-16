a7fbc0a0b8125e39c1e8e92f1b1aa38f1668fcde
GodwinCyber <godwinchuks032@gmail.com>
2024-01-16 08:21:52 -0800

    full description of the table first_table from the database hbtn_0c_0 in your MySQL server.

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- show full description of the table first_table from DB hbtn_0c_0 in your MySQL server
+-- the table name will be passed through an argument of the mysql command
+-- DESCRIBE or EXPLAIN statemenet not allowd
+SHOW CREATE TABLE first_table;
