c6be7807e2d3df4b7680c96c47c25c27cabab15a
GodwinCyber <godwinchuks032@gmail.com>
2024-01-16 06:03:50 -0800

    create a database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- create the database hbtn_0c_0 in MySQL server
+-- the script will should not fail even if hbtn_0c_0 already exist in MySQL
+-- SELECT and SHOW statement are not allowed
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
