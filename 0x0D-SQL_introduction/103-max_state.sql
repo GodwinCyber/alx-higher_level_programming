b173bdd8c4e9071a91f7c1543abd5c4f15ff3782
GodwinCyber <godwinchuks032@gmail.com>
2024-01-16 18:38:52 -0800

    display the max temp of each state

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- import hbtn_0c_0 database this table dump
 -- Display the maximum temp of each state (ordered by state name)
-SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state DESC LIMIT 3;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
