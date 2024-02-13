dab258c0028daee04701c1fe13d2223e6e1e9e09
GodwinCyber <godwinchuks032@gmail.com>
2024-02-13 15:16:11 +0000

    prints a message depending of the number of arguments passed:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const numArgs = process.argv.length - 2;
 
 // Print a message depending on the number of arguments
-if (numArgs == 0) {
+if (numArgs === 0) {
   console.log('No argument');
-} else if (numArgs == 1) {
+} else if (numArgs === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
