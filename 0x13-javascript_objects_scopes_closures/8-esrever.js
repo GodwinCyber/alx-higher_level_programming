91eedb737c3ec71b9d0433dfea13d4c07609c750
GodwinCyber <godwinchuks032@gmail.com>
2024-02-14 19:43:01 +0000

    function that prints the number of arguments already printed and the new argument value. (see example

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// function that returns the number of occurrences in a list:
+exports.esrever = function (list) {
+  const reversed = [];
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
+  }
+  return reversed;
+};
