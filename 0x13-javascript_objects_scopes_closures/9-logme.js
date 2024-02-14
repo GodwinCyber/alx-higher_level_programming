6fe17f2e0b2c01dc2a9ded1dd4570d29bb98ba23
GodwinCyber <godwinchuks032@gmail.com>
2024-02-14 20:09:18 +0000

    prints the number of arguments already printed and the new argument value

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -2,6 +2,6 @@
 // function that returns the number of occurrences in a list:
 let count = 0;
 exports.logMe = function (item) {
-  console.log(`${count} = ${item}`);
+  console.log(count + ': ' + item);
   count++;
 };
