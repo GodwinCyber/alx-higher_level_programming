2e70f19ca436a5c54927a231a6d837ee0548c9f0
GodwinCyber <godwinchuks032@gmail.com>
2024-02-15 04:18:26 +0000

    concate 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,26 +1,11 @@
 #!/usr/bin/node
-// Import the file system module
-const fs = require('fs');
-
+// concatenate 2 files
 const fileA = process.argv[2];
 const fileB = process.argv[3];
 const fileC = process.argv[4];
-
-fs.readFile(fileA, 'utf8', (err, dataA) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  fs.readFile(fileB, 'utf8', (err, dataB) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    const dataC = dataA + '\n' + dataB;
-    fs.writeFile(fileC, dataC, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-  });
-});
+const fs = require('fs');
+const w = fs.createWriteStream(fileC, { flags: 'a' });
+const r1 = fs.createReadStream(fileA);
+r1.pipe(w);
+const r2 = fs.createReadStream(fileB);
+r2.pipe(w);
