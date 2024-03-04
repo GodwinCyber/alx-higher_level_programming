7c393990fa478026c6a3628da7254a21b4d86ad2
GodwinCyber <godwinchuks032@gmail.com>
2024-03-04 15:28:51 -0800

    Python script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that takes your GitHub credentials (username and password)
+   and uses the GitHub API to display your id
+   to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    response = requests.get("http://api-github.com/user",
+                            auth=(username, password))
+    user = response.json()
+    print(user.get("id"))
