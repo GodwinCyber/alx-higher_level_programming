d4bc7755e4db019632fdd62120adb772600fb3d2
GodwinCyber <godwinchuks032@gmail.com>
2024-03-27 20:09:03 -0700

    script that computes the number of tasks completed by user id.

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error('Failed to get a successful response:', response.statusCode);
+    return;
+  }
+
+  const todos = JSON.parse(body);
+  const completedTasks = {};
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (!completedTasks[todo.userId]) {
+        completedTasks[todo.userId] = 0;
+      }
+      completedTasks[todo.userId]++;
+    }
+  });
+  console.log(completedTasks);
+});
